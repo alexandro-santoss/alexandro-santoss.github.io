d059c8e3feda857466e543d59c2500649e89f909
Alexandro Santos <165501555+alexandro-santoss@users.noreply.github.com>
2025-03-13 22:46:54 -0300

    Add files via upload

--- src/js/menu-mobile.js
+++ src/js/menu-mobile.js
@@ -1,16 +1,15 @@
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-navegacao-mobile');
-let overlay = document.getElementById('overlay-menu)
+let overlay = document.getElementById('overlay-menu')
 
 btnMenu.addEventListener('click', ()=> {
   menu.classList.add('abrir-menu-mob')
 })
 
-menu.addEventListener('click', () (
-  menu.classList.remove('abrir-menu-mo
-)
+menu.addEventListener('click', ()=> {
+  menu.classList.remove('abrir-menu-mob')
 })
 
-overlay.addEventListener('click, ))=> {
-  menu.classList.remove('abrir-menu-mob
+overlay.addEventListener('click', ()=> {
+  menu.classList.remove('abrir-menu-mob')
 })
